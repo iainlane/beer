699f89b0f04e7658c8c5b0dbf228df303a9a22e3
Iain Lane <iain@orangesquash.org.uk>
2020-04-18 09:44:52 +0100

    remove a test file

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/upload', uploadRouter);
 app.use('/images', imagesRouter);
-app.use('/json', jsonRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
